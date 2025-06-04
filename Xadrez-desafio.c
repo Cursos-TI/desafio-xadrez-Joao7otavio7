8de133336d99cb28b36f33a04a136e511c8526df
Joao7otavio7 <joaootavio569@hotmail.com>
2025-06-04 23:41:24 +0000

    Desafio xadrez1.0

--- Xadrez-desafio.c
+++ Xadrez-desafio.c
@@ -6,6 +6,7 @@ int torre, bispo, rainha, direcaotorre, direcaobisbo, direcaorainha;
 int avancotorre = 1;
 int avancobisbo = 1;
 
+
 printf ("Para qual direção deseja mover a torre?\n 1. Vertical\n 2. horizontal\n");
 scanf ("%d", &direcaotorre);
 
@@ -37,7 +38,7 @@ default:
 }
 
 printf ("\n");
-printf ("Para qual direção deseja mover o bisbo?\n 1. Cima/Direita\n 2. Cima/esquerda\n");
+printf ("Para qual direção deseja mover o bisbo?\n 1. Cima/Direita\n 2. Cima/Esquerda\n");
 scanf ("%d", &direcaobisbo);
 
 switch (direcaobisbo)
@@ -66,13 +67,52 @@ default:
 }
 
 printf ("\n");
-printf ("Para qual direção deseja mover o bisbo?\n 1. Cima/Direita\n 2. Cima/esquerda\n");
+printf ("Para qual direção deseja mover o bisbo?\n 1. Cima\n 2. Esquerda\n 3. Direita\n 4. Baixo\n 5. Cima/Esquerda\n 6. Cima/Direita\n");
 scanf ("%d", &direcaorainha);
 
 switch (direcaorainha)
 {
 case 1:
-    /* code */
+   printf ("Insira quantas casas deseja avançar com a rainha. \n");
+scanf ("%d", &rainha);
+        for (int avancorainha = 1; avancorainha <= rainha; avancorainha++) {
+            printf ("%d Cima\n", avancorainha);
+        }
+    break;
+case 2:
+      printf ("Insira quantas casas deseja avançar com a rainha. \n");
+scanf ("%d", &rainha);
+        for (int avancorainha = 1; avancorainha <= rainha; avancorainha++) {
+            printf ("%d Esquerda\n", avancorainha);
+        }
+    break;
+case 3:
+       printf ("Insira quantas casas deseja avançar com a rainha. \n");
+scanf ("%d", &rainha);
+        for (int avancorainha = 1; avancorainha <= rainha; avancorainha++) {
+            printf ("%d Direita\n", avancorainha);
+        }
+    break;
+case 4:
+       printf ("Insira quantas casas deseja avançar com a rainha. \n");
+scanf ("%d", &rainha);
+        for (int avancorainha = 1; avancorainha <= rainha; avancorainha++) {
+            printf ("%d Baixo\n", avancorainha);
+        }
+    break;
+case 5:
+       printf ("Insira quantas casas deseja avançar com a rainha. \n");
+scanf ("%d", &rainha);
+        for (int avancorainha = 1; avancorainha <= rainha; avancorainha++) {
+            printf ("%d Cima/Esquerda\n", avancorainha);
+        }
+    break;
+case 6:
+      printf ("Insira quantas casas deseja avançar com a rainha. \n");
+scanf ("%d", &rainha);
+        for (int avancorainha = 1; avancorainha <= rainha; avancorainha++) {
+            printf ("%d Cima/Direita\n", avancorainha);
+        }
     break;
 
 default:
