d984b6260b935dd24e2a266e256339aae0988936
Joao7otavio7 <joaootavio569@hotmail.com>
2025-06-05 01:50:41 +0000

    Desafio Xadrez versão final

--- Xadrez-desafio.c
+++ Xadrez-desafio.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
+// Recursividade para chamar o movimento da torre.
 void movimentoTorre () {
-    int torre, direcaotorre, avancotorre = 1;
+    int torre, direcaotorre, avancotorre = 1; //Variaveis para chamar ações na torre
 
     printf ("Para qual direção deseja mover a torre?\n 1. Vertical\n 2. horizontal\n");
 scanf ("%d", &direcaotorre);
 
-switch (direcaotorre)
+switch (direcaotorre) // Menu para selecionar as direções da torre
 {
 case 1:
     printf ("Insira quantas casas deseja avançar com a torre. \n");
@@ -35,15 +35,15 @@ default:
 
 
 }
-
+// Recursividade para chamar o movimento da Bispo.
 void movimentoBispo () {
-    int bispo, direcaobispo;
+    int bispo, direcaobispo; //Variaveis para chamar ações na Bispo
 
 printf ("\n");
 printf ("Para qual direção deseja mover o bisbo?\n 1. Cima/Direita\n 2. Cima/Esquerda\n");
 scanf ("%d", &direcaobispo);
 
-switch (direcaobispo)
+switch (direcaobispo) // Menu para selecionar as direções da bispo
 {
 case 1:
 printf ("Insira quantas casas deseja avançar com a bispo. \n");
@@ -75,14 +75,14 @@ default:
 }
 
 }
-
+// Recursividade para chamar o movimento da Rainha.
 void movimentoRainha () {
-    int rainha, direcaorainha;
+    int rainha, direcaorainha; //Variaveis para chamar ações na Rainha
     printf ("\n");
 printf ("Para qual direção deseja mover a rainha?\n 1. Cima\n 2. Esquerda\n 3. Direita\n 4. Baixo\n 5. Cima/Esquerda\n 6. Cima/Direita\n");
 scanf ("%d", &direcaorainha);
 
-switch (direcaorainha)
+switch (direcaorainha) // Menu para selecionar as direções da rainha
 {
 case 1:
    printf ("Insira quantas casas deseja avançar com a rainha. \n");
@@ -136,18 +136,25 @@ default:
 int main() {
 
 
-int direcaocavalo, cavalo = 1;
+int direcaocavalo, cavalo = 1; //Variaveis para chamar ações no cavalo
 
+// Apresentação
+
+ printf("===================================\n");
+    printf("         JOGO DE XADREZ            \n");
+    printf("===================================\n");
+
+// Recursividade Torre
 movimentoTorre();
-
+// Recursividade Bispo
 movimentoBispo();
-
+// Recursividade Rainha
 movimentoRainha ();
 
 printf ("Para qual direção deseja mover o cavalo?\n 1. Cima/Direta\n 2. Cima/Esquerda\n");
 scanf ("%d", &direcaocavalo);
 
-switch (direcaocavalo)
+switch (direcaocavalo) // Menu para escolher a direção do cavalo.
 {
 case 1:
     while (cavalo--)
